a9916864f2baacc3f4d9d194810e4be60a48c063
exmyth <iexmyth@gmail.com>
2016-01-02 23:52:51 +0800

    cancel no use import

--- src/com/exmyth/wechat/MainActivity.java
+++ src/com/exmyth/wechat/MainActivity.java
@@ -2,8 +2,6 @@ package com.exmyth.wechat;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
