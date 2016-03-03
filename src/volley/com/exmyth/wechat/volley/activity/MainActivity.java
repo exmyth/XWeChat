3af0855b1bf4e72b88f86eed6a84458d74dd252f
exmyth <iexmyth@gmail.com>
2016-03-03 17:27:06 +0800

    add xmlrequest

--- src/volley/com/exmyth/wechat/volley/activity/MainActivity.java
+++ src/volley/com/exmyth/wechat/volley/activity/MainActivity.java
@@ -1,16 +1,20 @@
 package com.exmyth.wechat.volley.activity;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request.Method;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.XMLRequest;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.ImageLoader.ImageListener;
 import com.android.volley.toolbox.ImageRequest;
@@ -50,9 +54,46 @@ public class MainActivity extends Activity {
 //		requestJsonArray();
 //		requestImage();
 //		loadImage();
-		setNetworkImageView();
+//		setNetworkImageView();
+		requestXML();
 	}
 	
+	private void requestXML() {
+		XMLRequest xmlRequest = new XMLRequest(  
+		        "http://flash.weather.com.cn/wmaps/xml/china.xml",  
+		        new Response.Listener<XmlPullParser>() {  
+		            @Override  
+		            public void onResponse(XmlPullParser response) {  
+		                try {  
+		                    int eventType = response.getEventType();  
+		                    while (eventType != XmlPullParser.END_DOCUMENT) {  
+		                        switch (eventType) {  
+		                        case XmlPullParser.START_TAG:  
+		                            String nodeName = response.getName();  
+		                            if ("city".equals(nodeName)) {  
+		                                String pName = response.getAttributeValue(0);  
+		                                Log.d("TAG", "pName is " + pName);  
+		                            }  
+		                            break;  
+		                        }  
+		                        eventType = response.next();  
+		                    }  
+		                } catch (XmlPullParserException e) {  
+		                    e.printStackTrace();  
+		                } catch (IOException e) {  
+		                    e.printStackTrace();  
+		                }  
+		            }  
+		        }, new Response.ErrorListener() {  
+		            @Override  
+		            public void onErrorResponse(VolleyError error) {  
+		                Log.e("myth", error.getMessage(), error);  
+		            }  
+		        });  
+		xmlRequest.setShouldCache(false);
+		mQueue.add(xmlRequest);
+	}
+
 	private void setNetworkImageView() {
 		networkImageView = (NetworkImageView) findViewById(R.id.network_image_view);
 		
