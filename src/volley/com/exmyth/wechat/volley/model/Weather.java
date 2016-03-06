d925c09c6ca441d8c5b85186cc98ff168583ad66
exmyth <iexmyth@gmail.com>
2016-03-06 22:00:06 +0800

    add volley gson resolver

---
+++ src/volley/com/exmyth/wechat/volley/model/Weather.java
@@ -0,0 +1,14 @@
+package com.exmyth.wechat.volley.model;
+public class Weather {  
+  
+    private WeatherInfo weatherinfo;  
+  
+    public WeatherInfo getWeatherinfo() {  
+        return weatherinfo;  
+    }  
+  
+    public void setWeatherinfo(WeatherInfo weatherinfo) {  
+        this.weatherinfo = weatherinfo;  
+    }  
+  
+} 
