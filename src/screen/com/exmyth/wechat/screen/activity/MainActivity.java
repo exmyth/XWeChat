428b1984cf07db671b20a32d82787a9916e57170
zhongmaming <zhongmaming@uni-ubi.com>
2016-04-07 16:52:52 +0800

    full screen param

---
+++ src/screen/com/exmyth/wechat/screen/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.exmyth.wechat.screen.activity;
+
+import com.exmyth.wechat.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.screen_activity_main);
+	}
+}
